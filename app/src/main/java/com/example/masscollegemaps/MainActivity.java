6d94b1676f6e6cc545ea6080fce9b237110284a7
twinstarjoe5754 <jbarry6@worcester.edu>
2023-04-30 09:12:42 -0400

    after getting songs to play: working on UI, Actionbar, preferences

--- app/src/main/java/com/example/masscollegemaps/MainActivity.java
+++ app/src/main/java/com/example/masscollegemaps/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.masscollegemaps;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-//import android.view.LayoutInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,33 +23,77 @@ public class MainActivity extends AppCompatActivity {
             "Antigua",
             "Australia",
             "Barbados",
+            "Brazil",
             "Canada",
+            "Denmark",
+            "Egypt",
+            "France",
+            "Germany",
+            "Greece",
+            "Iceland",
             "Ireland",
+            "Italy",
             "Jamaica",
             "Mexico",
             "St. Kitts and Nevis",
-            "England",
+            "United Kingdom",
             "United States",
     };
     int[] images = new int[]{
             R.drawable.flag_of_antigua_and_barbuda,
             R.drawable.flag_of_australia,
             R.drawable.flag_of_barbados,
+            R.drawable.flag_of_brazil,
             R.drawable.flag_of_canada,
+            R.drawable.flag_of_denmark,
+            R.drawable.flag_of_egypt,
+            R.drawable.flag_of_france,
+            R.drawable.flag_of_germany,
+            R.drawable.flag_of_greece,
+            R.drawable.flag_of_iceland,
             R.drawable.flag_of_ireland,
+            R.drawable.flag_of_italy,
             R.drawable.flag_of_jamaica,
             R.drawable.flag_of_mexico,
             R.drawable.flag_of_saint_kitts_and_nevis,
             R.drawable.flag_of_the_united_kingdom,
             R.drawable.flag_of_the_united_states
     };
+    int[] anthems = new int[]{
+            R.raw.ag,
+            R.raw.au,
+            R.raw.bb,
+            R.raw.br,
+            R.raw.ca,
+            R.raw.dk,
+            R.raw.eg,
+            R.raw.fr,
+            R.raw.ar,
+            R.raw.gr,
+            R.raw.is,
+            R.raw.ie,
+            R.raw.it,
+            R.raw.jm,
+            R.raw.mx,
+            R.raw.kn,
+            R.raw.gb,
+            R.raw.us
+    };
+
     LatLng[] selectedCoordinates = new LatLng[]{
             new LatLng(17.080817709376724, -61.82930247684425), // antigua
-
-            new LatLng(-33.855619455344026, 151.1788475689928), // sydney australia
+            new LatLng(-33.855619455344026, 151.1788475689928), // australia sydney
             new LatLng(13.102379216842754, -59.59302847298943), // Barbados
+            new LatLng(-15.78333333, -47.916667),               // Brazil (Brazilia)
             new LatLng(45.50627310620357,  -73.61642962581581), // Montreal Canada
-            new LatLng(53.329609113076934, -6.280091935482406), // Dublin Ireland
+            new LatLng(55.66666667,     12.583333),             // Denmark( Copenhagen)
+            new LatLng(30.05,           31.25),                 // Egypt (cairo )
+            new LatLng(48.86666667,  2.333333),                 // france (Paris)
+            new LatLng(52.51666667,  13.4),                     // germany (Berlin)
+            new LatLng(37.98333333,  23.733333),                // greece (Athens)
+            new LatLng(64.15,  -21.95),                         // Iceland (Reykjavik )
+            new LatLng(53.329609113076934, -6.280091935482406), // Ireland Dublin
+            new LatLng(41.9, 12.483333),                        // Italy Rome
             new LatLng(18.0491678267187,   -76.82120673837873), // Kingston Jamaica
             new LatLng(19.446155649781648, -99.10935478364311), // Mexico City
             new LatLng(17.140667016809264, -62.62301457927994), // St. Kitts and Nevis
@@ -76,10 +118,12 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String selectedName = names[position];
                 int selectedImage = images[position];
+                int selectedAnthem = anthems[position];
                 LatLng selectedGPS = selectedCoordinates[position];
                 startActivity(new Intent(MainActivity.this, MapsActivity.class).
                         putExtra("name", selectedName).
                         putExtra("image", selectedImage).
+                        putExtra("anthem", selectedAnthem).
                         putExtra("latitude", selectedGPS.latitude).
                         putExtra("longitude", selectedGPS.longitude));
             }
@@ -88,9 +132,9 @@ public class MainActivity extends AppCompatActivity {
 
     public class CustomAdapter extends BaseAdapter {
 
+        private Context context;
         private String[] imageNames;
         private int[] imagesPhoto;
-        private Context context;
         private LayoutInflater layoutInflater;
 
         public CustomAdapter(String[] imageNames, int[] imagesPhoto, Context context) {
@@ -104,17 +148,14 @@ public class MainActivity extends AppCompatActivity {
         public int getCount() {
             return imagesPhoto.length;
         }
-
         @Override
         public Object getItem(int i) {
             return null;
         }
-
         @Override
         public long getItemId(int i) {
             return 0;
         }
-
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             if (view == null) {
--- app/src/main/java/com/example/masscollegemaps/MapsActivity.java
+++ app/src/main/java/com/example/masscollegemaps/MapsActivity.java
@@ -3,10 +3,10 @@ package com.example.masscollegemaps;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,21 +14,29 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.masscollegemaps.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    MediaPlayer mediaPlayer;
 
     private GoogleMap mMap;
     private FrameLayout map;
 
-    String selectedName;
-    int selectedImage;
-    double selectedLong;
-    double selectedLat;
+    String  selectedName;
+    int     selectedImage;
+    int     selectedAnthem;
+    double  selectedLong;
+    double  selectedLat;
+
+    Handler handler;
+    Runnable runnable;
+    final long UPDATE_MILLIS = 30;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mediaPlayer = null;
 
         setContentView(R.layout.activity_maps);
         map = findViewById(R.id.map);
@@ -37,6 +45,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (intent.getExtras() != null){
             selectedName      = intent.getStringExtra("name");
             selectedImage     = intent.getIntExtra("image", 0);
+            selectedAnthem    = intent.getIntExtra("anthem", 0);
             selectedLong      = intent.getDoubleExtra("longitude", 0);
             selectedLat       = intent.getDoubleExtra("latitude", 0);
 
@@ -51,11 +60,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        playAnthem();
         setTitle(selectedName);
         mMap = googleMap;
-
         LatLng location = new LatLng(selectedLat, selectedLong);
         mMap.addMarker(new MarkerOptions().position(location).title(selectedName));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+    private void playAnthem() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, selectedAnthem);
+        }
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.stop();
+            }
+        });
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+         super.onStop();
+    }
+    @Override
+    protected void onPause() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+          }
+        super.onPause();
     }
 }
